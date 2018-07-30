7099d737534a3a692759301caa4b5ef40523b9e4
Benjamin Otto <benjamin.otto@style-systems.de>
2018-07-30 16:08:37 +0200

    updated deps

--- packages/pwac-react/src/babel.ts
+++ packages/pwac-react/src/babel.ts
@@ -10,11 +10,38 @@ export const getTsRule = (
     {
       loader: 'babel-loader',
       options: {
-        plugins: (!production ? ['react-hot-loader/babel'] : []).concat(
+        plugins: (!production
+          ? ['react-hot-loader/babel']
+          : ([] as any)
+        ).concat([
           'react-loadable/babel',
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-proposal-object-rest-spread'
-        ),
+          // Stage 0
+          '@babel/plugin-proposal-function-bind',
+
+          // Stage 1
+          '@babel/plugin-proposal-export-default-from',
+          '@babel/plugin-proposal-logical-assignment-operators',
+          ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+          ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+          [
+            '@babel/plugin-proposal-nullish-coalescing-operator',
+            { loose: false }
+          ],
+          '@babel/plugin-proposal-do-expressions',
+
+          // Stage 2
+          ['@babel/plugin-proposal-decorators', { legacy: true }],
+          '@babel/plugin-proposal-function-sent',
+          '@babel/plugin-proposal-export-namespace-from',
+          '@babel/plugin-proposal-numeric-separator',
+          '@babel/plugin-proposal-throw-expressions',
+
+          // Stage 3
+          '@babel/plugin-syntax-dynamic-import',
+          '@babel/plugin-syntax-import-meta',
+          ['@babel/plugin-proposal-class-properties', { loose: false }],
+          '@babel/plugin-proposal-json-strings'
+        ]),
         presets: [
           [
             '@babel/preset-env',
@@ -27,10 +54,6 @@ export const getTsRule = (
             }
           ],
           '@babel/preset-react',
-          [
-            '@babel/preset-stage-0',
-            { decoratorsLegacy: true, pipelineProposal: 'minimal' }
-          ],
           '@babel/preset-typescript'
         ]
       }
